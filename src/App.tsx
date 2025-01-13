017cfd3385f10f48ab46e13e20085ed44c0515f1
mohak <c.mohak2004@gmail.com>
2025-01-13 23:39:53 +0530

    fe update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import Phaser, { Physics, Scene } from 'phaser';
+import MainScene from './scenes/MainScene';
 
-import './App.css'
+
 
 function App() {
 
+  useEffect(() => {
+
+    const config = {
+      type : Phaser.AUTO,
+      width : 800,
+      height : 600,
+      Physics : {
+        default : 'arcade',
+        arcade : {
+          gravity : { y : 0}
+        }
+      },
+      Scene : MainScene
+    };
+
+    const game = new Phaser.Game(config);
+
+    return () => {
+      game.destroy(true);
+    }
+  }, [])
 
   return (
     <>
+    <div id='game-container'></div>
     </>
   )
 }
